10c9c643dd937315b27b46ef37a3685f95905d17
iadmin08 <iadmin08@gmail.com>
2017-08-05 00:22:18 -0400

    Initial commit

---
+++ lab1/sample-rest-service/src/main/java/iadmin08/labs/spring/boot/lab1/samplerestservice/SampleRestServiceApplication.java
@@ -0,0 +1,12 @@
+package iadmin08.labs.spring.boot.lab1.samplerestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestServiceApplication.class, args);
+	}
+}
